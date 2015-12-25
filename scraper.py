074bd087ca9420aea763dfb2b043ba7a53ed41c3
jennyj5 <jennyj5@mit.edu>
2015-12-25 18:14:19 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,15 +3,15 @@ from scrapemark import scrape
 import scraperwiki
 
 for page in range(1,40):
-  #URL = "https://appexchange.salesforce.com/results?pageNo="+str(page)+"&filter=a0L3000000OvSOEEA3" #Sales
-  #URL = "https://appexchange.salesforce.com/results?pageNo="+str(page)+"&filter=a0L3000000OvSOFEA3" #Customer Service
+  URL = "https://appexchange.salesforce.com/results?pageNo="+str(page)+"&filter=a0L3000000OvSOEEA3" #Sales
+  URL = "https://appexchange.salesforce.com/results?pageNo="+str(page)+"&filter=a0L3000000OvSOFEA3" #Customer Service
   URL = "https://appexchange.salesforce.com/results?pageNo="+str(page)+"&filter=a0L3000000OvSOGEA3" #Marketing
-  #URL = "https://appexchange.salesforce.com/results?pageNo="+str(page)+"&filter=a0L3000000OvSOHEA3" #IT & Admin
-  #URL = "https://appexchange.salesforce.com/results?pageNo="+str(page)+"&filter=a0L3000000OvSOIEA3" #HR
-  #URL = "https://appexchange.salesforce.com/results?pageNo="+str(page)+"&filter=a0L3000000OvSOJEA3" #Finance
-  #URL = "https://appexchange.salesforce.com/results?pageNo="+str(page)+"&filter=a0L3000000OvSOKEA3" #ERP
-  #URL = "https://appexchange.salesforce.com/results?pageNo="+str(page)+"&filter=a0L3000000OvSOLEA3" #Collabration
-  #URL = "https://appexchange.salesforce.com/category/analytics" #Analytics
+  URL = "https://appexchange.salesforce.com/results?pageNo="+str(page)+"&filter=a0L3000000OvSOHEA3" #IT & Admin
+  URL = "https://appexchange.salesforce.com/results?pageNo="+str(page)+"&filter=a0L3000000OvSOIEA3" #HR
+  URL = "https://appexchange.salesforce.com/results?pageNo="+str(page)+"&filter=a0L3000000OvSOJEA3" #Finance
+  URL = "https://appexchange.salesforce.com/results?pageNo="+str(page)+"&filter=a0L3000000OvSOKEA3" #ERP
+  URL = "https://appexchange.salesforce.com/results?pageNo="+str(page)+"&filter=a0L3000000OvSOLEA3" #Collabration
+  URL = "https://appexchange.salesforce.com/category/analytics" #Analytics
   
   
   print URL
